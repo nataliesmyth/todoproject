2001c0058a28468a6e20f63e4efa12527f2696ba
Nataliesmyth <nataliesmyth11@gmail.com>
2021-04-13 01:08:42 -0400

    server wont run

--- todoproject/toDoApp/apps.py
+++ todoproject/toDoApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TodoappConfig(AppConfig):
+class todoappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'toDoApp'
+    name = 'todoapp'
--- todoproject/toDoApp/urls.py
+++ todoproject/toDoApp/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include # import include
 
 urlpatterns = [
-    path(' ',views.index,name="index")
+    path('admin/', admin.site.urls),
+    path('', include('todoapp.urls'))  # add this line
 ]
