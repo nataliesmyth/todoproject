b88441842108e08e6daee20ecfdf616df36cfc92
Nataliesmyth <nataliesmyth11@gmail.com>
2021-04-17 21:12:08 -0400

    added notes and comments

--- todoproject/todolist/views.py
+++ todoproject/todolist/views.py
@@ -6,3 +6,6 @@ def index(request):
     # return HttpResponse("Hello World!!")
     form = TaskForm()
     return render(request, "index.html", {"task_form": form})
+
+# We built a form instance with task_form = TaskForm() & pass our form in our template index.html with the context {"task_form":form}
+# In our template, we can access this variable with task_form key
