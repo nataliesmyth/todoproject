944f087c7d04aeb85a06a2a27700ba6bc12ffe91
Nataliesmyth <nataliesmyth11@gmail.com>
2021-04-17 23:29:18 -0400

    error in form styling

--- todoproject/todolist/forms.py
+++ todoproject/todolist/forms.py
@@ -9,6 +9,20 @@ class TaskForm(forms.ModelForm):
         fields = "__all__"  # include all fields in form
         # fields=('title','completed') # include particular fileds of model in form
 
+    title = forms.Charfield(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control form-control-lg",
+                "placeholder": "Enter a task...",
+            }
+        ),
+    )
+
+    completed = forms.CharField(
+        required=False,
+        widget=forms.widgets.CheckboxInput(attrs={"class": "form-check-input"}),
+    )
+
 
 # To create a form from a model, you just need to indicate which model to use to build the form in the Meta class of the form.
 # Django will them introspect the model and build the form dynamically
