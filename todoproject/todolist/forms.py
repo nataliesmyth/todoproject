58394ac59feb05261c8fbc773bf8fb9db5e1ce42
Nataliesmyth <nataliesmyth11@gmail.com>
2021-04-17 23:33:01 -0400

    fixed styling form errors on single item page

--- todoproject/todolist/forms.py
+++ todoproject/todolist/forms.py
@@ -7,13 +7,13 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = "__all__"  # include all fields in form
-        # fields=('title','completed') # include particular fileds of model in form
+        # fields = ("title",)  # include particular fileds of model in form
 
-    title = forms.Charfield(
+    title = forms.CharField(
         widget=forms.TextInput(
             attrs={
                 "class": "form-control form-control-lg",
-                "placeholder": "Enter a task...",
+                "placeholder": "Enter task...",
             }
         ),
     )
@@ -22,7 +22,3 @@ class TaskForm(forms.ModelForm):
         required=False,
         widget=forms.widgets.CheckboxInput(attrs={"class": "form-check-input"}),
     )
-
-
-# To create a form from a model, you just need to indicate which model to use to build the form in the Meta class of the form.
-# Django will them introspect the model and build the form dynamically
