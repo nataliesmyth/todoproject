f7bf62caccbd90029bfce48ffa301dd5dedd60ee
Nataliesmyth <nataliesmyth11@gmail.com>
2021-04-17 22:00:02 -0400

    added url pattern for updating a task

--- todoproject/todolist/urls.py
+++ todoproject/todolist/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name="index")
+    path('',views.index,name="index"),
+    path('update/<int:pok>/', views.update_task, name="update task"),
 ]
