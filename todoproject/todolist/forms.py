d20bb566ba265fe4cdbaa02e2b05a5b6fe163dd3
Nataliesmyth <nataliesmyth11@gmail.com>
2021-04-17 21:28:59 -0400

    form rendered in browser

--- todoproject/todolist/forms.py
+++ todoproject/todolist/forms.py
@@ -1,16 +1,13 @@
-# Form: allows you to build standard forms
-# ModelForm: Allows you to build forms tied to model instances
-
 from django import forms
 from django.forms import ModelForm
 from .models import Task
 
+
 class TaskForm(forms.ModelForm):
     class Meta:
-        # include all fields in form
-        model = Taskfields= "__all__"
-        # include particular  fields of model in form
-        # fields=('title,completed')
+        model = Task
+        fields = "__all__"  # include all fields in form
+        # fields=('title','completed') # include particular fileds of model in form
 
 
 # To create a form from a model, you just need to indicate which model to use to build the form in the Meta class of the form.
--- todoproject/todolist/views.py
+++ todoproject/todolist/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, HttpResponse
 from .forms import TaskForm
 
 # Create your views here.
+
+
 def index(request):
     # return HttpResponse("Hello World!!")
     form = TaskForm()
