f277d75bd69661d4a07841cc90f3313c25b67f48
Nataliesmyth <nataliesmyth11@gmail.com>
2021-04-17 21:10:18 -0400

    built form instance and passed form in specified template

--- todoproject/todolist/views.py
+++ todoproject/todolist/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse
+from .forms import TaskForm
 
 # Create your views here.
 def index(request):
     # return HttpResponse("Hello World!!")
-    return render(request, "index.html")
+    form = TaskForm()
+    return render(request, "index.html", {"task_form": form})
