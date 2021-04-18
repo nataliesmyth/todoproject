d6e22f9497f5ee8f27fb80d9e208f3416ca75223
Nataliesmyth <nataliesmyth11@gmail.com>
2021-04-17 22:01:18 -0400

    added notes

--- todoproject/todolist/urls.py
+++ todoproject/todolist/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name="index"),
+    # takes the pk arguments and maps it to the update_task view
     path('update/<int:pok>/', views.update_task, name="update task"),
 ]
