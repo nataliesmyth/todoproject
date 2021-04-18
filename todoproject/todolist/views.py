009ba06d5e82a4e6efd652d7df3730ee9a0368a0
Nataliesmyth <nataliesmyth11@gmail.com>
2021-04-17 21:33:40 -0400

    add functionality for users to CRUD tasks in browser

--- todoproject/todolist/views.py
+++ todoproject/todolist/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import redirect, render, HttpResponse
 from .forms import TaskForm
 
 # Create your views here.
@@ -7,6 +7,13 @@ from .forms import TaskForm
 def index(request):
     # return HttpResponse("Hello World!!")
     form = TaskForm()
+
+    if request.method == "POST":
+        # Get the posted form
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("index")
     return render(request, "index.html", {"task_form": form})
 
 # We built a form instance with task_form = TaskForm() & pass our form in our template index.html with the context {"task_form":form}
