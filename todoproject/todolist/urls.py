dfb559f9d39c8d5ee91426db4e60616277e1ec43
Nataliesmyth <nataliesmyth11@gmail.com>
2021-04-17 22:11:36 -0400

    fixed errors so update button appears next to each task in browser

--- todoproject/todolist/urls.py
+++ todoproject/todolist/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name="index"),
-    # takes the pk arguments and maps it to the update_task view
-    path('update/<int:pok>/', views.update_task, name="update task"),
+    path("", views.index, name="index"),
+    path("update/<int:pk>/", views.update_task, name="update_task"),
 ]
